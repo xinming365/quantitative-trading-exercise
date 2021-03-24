16ad42f32b4843e4088e20529c8733a577667828
xinming365 <xinming_365@163.com>
2021-03-24 10:29:21 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # quantitative-trading-exercise
 collections of tricks, plots, and projects in the quantitative trading (QT) field.
+
+
+
+### 环境安装
+
+推荐使用conda安装单独的运行环境，使用quant.yaml配置文件
+
+> conda  env  create  -f quant.yaml
+
+如果使用Pip包管理工具，则使用下面的方式安装必须的包
+
+> pip install -r quant.txt
+
+
+
--- plot_projects/plot_ohlc.py
+++ plot_projects/plot_ohlc.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 
 plt.rcParams['figure.figsize']=(18,6)
 sns.set_style('darkgrid', {'font.sans-serif':['SimHei', 'Arial']})
-df_stock = pd.read_excel('./data/something.xlsx')
+filename = 'F:\GitHub\My_data_set\I/20131018.csv'
+df_stock = pd.read_csv(filename)
 
 new_df = df_stock.loc[:, ['开盘', '最高', '最低', '收盘']]
 sns.lineplot(data=new_df.iloc[:150])
